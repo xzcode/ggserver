2353b60fc9326d516146284f0ea7943ff3e8575e
xz <379551875@qq.com>
2019-05-24 19:32:58 +0800

    添加牌九牌值定义

---
+++ ggserver-game/src/main/java/xzcode/ggserver/game/common/algorithms/pai/jiu/PaiJiuAlgoUtil.java
@@ -0,0 +1,33 @@
+package xzcode.ggserver.game.common.algorithms.pai.jiu;
+
+public interface PaiJiuAlgoUtil {
+	
+	public static int[] PAI_JIU_VALUE_ARRAY = {
+			10030,//丁三
+			10040,//二四
+			10051,//杂5-1
+			10052,//杂5-2
+			10071,//杂7-1
+			10072,//杂7-2
+			10081,//杂8-1
+			10082,//杂8-2
+			10091,//杂9-1
+			10092,//杂9-2
+			11060,//霖零六
+			11070,//高脚七
+			11100,//红头十
+			11110,//斧头
+			12020,//板凳
+			12060,//长三
+			13100,//梅牌
+			14040,//鹅牌
+			14080,//人牌
+			15020,//地牌
+			15120,//天牌
+	};
+	
+	public static void main(String[] args) {
+		System.out.println(1031 % 1000);
+	}
+	
+}
