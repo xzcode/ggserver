d77210fac976bd9f8854ad813e3afb3fdd948960
chenjianren <13302677051@163.com>
2019-09-10 19:52:55 +0800

    小廖：优化buf代码

--- ggserver-core/src/main/java/xzcode/ggserver/core/handler/serializer/impl/ProtobufSerializer.java
+++ ggserver-core/src/main/java/xzcode/ggserver/core/handler/serializer/impl/ProtobufSerializer.java
@@ -1,6 +1,12 @@
 package xzcode.ggserver.core.handler.serializer.impl;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.google.protobuf.Descriptors;
+import com.google.protobuf.Message;
 
 import io.protostuff.LinkedBuffer;
 import io.protostuff.ProtostuffIOUtil;
@@ -19,15 +25,19 @@ public class ProtobufSerializer  implements ISerializer {
 			log.error("Failed to deserialize, byte is empty");
 			throw new RuntimeException("Failed to deserialize");
 		}
-
-		T instance;
+		T instance = null;
+		// 反射
 		try {
-			instance = targetClass.newInstance();
-		} catch (InstantiationException | IllegalAccessException e) {
-			throw new RuntimeException("Failed to deserialize");
+			Method method = targetClass.getMethod("newBuilder");
+			Object obj = method.invoke(null, new Object[] {});
+			Message.Builder msgBuilder = (Message.Builder) obj;
+			Map<String, String> map=new HashMap<>();
+			map.put("token", "eeee");
+			map.put("gameId", "11");
+			instance=(T)buildMessage(msgBuilder,map);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		Schema<T> schema = RuntimeSchema.getSchema(targetClass);
-		ProtostuffIOUtil.mergeFrom(paramArrayOfByte, instance, schema);
 		return instance;
 	}
 
@@ -51,5 +61,59 @@ public class ProtobufSerializer  implements ISerializer {
 			buffer.clear();
 		}
 		return protoStuff;
+	}
+	public static Message buildMessage(Message.Builder msgBuilder,Map<String, String> map) {
+		Descriptors.Descriptor descriptor = msgBuilder.getDescriptorForType();
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+			Descriptors.FieldDescriptor filedDescriptor = descriptor.findFieldByName(entry.getKey());
+			if (filedDescriptor == null) {
+				continue;
+			}
+			boolean isRepeated = filedDescriptor.isRepeated();
+			Descriptors.FieldDescriptor.JavaType type = filedDescriptor.getJavaType();
+			if (isRepeated) {
+				String value = entry.getValue();
+				String[] strArray = value.split(",");
+				for (int i = 0; i < strArray.length; ++i) {
+					Object valueObject = getObject(strArray[i], type); // getObject
+					if (valueObject == null) {
+						continue;
+					}
+					msgBuilder.addRepeatedField(filedDescriptor, valueObject);
+				}
+			} else {
+				Object valueObject = getObject(entry.getValue(), type);
+				if (valueObject == null) {
+					continue;
+				}
+				msgBuilder.setField(filedDescriptor,
+						getObject(entry.getValue(), type));
+			}
+		}
+		return msgBuilder.build();
+	}
+
+	private static Object getObject(String rawString,Descriptors.FieldDescriptor.JavaType type) {
+		try {
+			switch (type) {
+			case INT:
+				return Integer.valueOf(rawString);
+			case LONG:
+				return Long.valueOf(rawString);
+			case FLOAT:
+				return Float.valueOf(rawString);
+			case DOUBLE:
+				return Double.valueOf(rawString);
+			case BOOLEAN:
+				return Boolean.valueOf(rawString);
+			case STRING:
+				return rawString;
+			default:
+				return null;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
